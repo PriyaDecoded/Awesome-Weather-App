871c20b35c65316ec383ce0d954b18c3d54f3fa7
PriyaDecoded <111013392+PriyaDecoded@users.noreply.github.com>
2022-09-25 12:58:29 -0400

    Update Time

--- Week7Project/src/index.js
+++ Week7Project/src/index.js
@@ -1,3 +1,19 @@
+function formatDate(timestamp){
+    let date = new Date;
+    let hours = date.getHours();
+    if (hours < 10){
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response){
     console.log(response.data);
     let cityElement = document.querySelector("#city");
@@ -10,6 +26,8 @@ function displayTemperature(response){
     humidElement.innerHTML = response.data.currentConditions.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = response.data.currentConditions.windspeed;
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.currentConditions.datetimeEpoch*1000);
 
 }
 
